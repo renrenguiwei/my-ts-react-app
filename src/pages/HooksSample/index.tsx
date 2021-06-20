b2ddb461299f76c714919bb8c65cd4c8eec9487d
ClausWong <726787295@qq.com>
2021-06-20 19:28:20 +0800

    useEffect多依赖进，触发次数

--- src/pages/HooksSample/index.tsx
+++ src/pages/HooksSample/index.tsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from 'react'
 
 const HooksSample = () => {
   const [count, setCount] = useState(0)
+  const name = 'claus'
   const todos = [{ text: 'myName' }]
   useEffect(() => {
     console.log('todos change')
-  }, [todos])
+  }, [count, name])
+
+  setTimeout(() => {
+    setCount(1)
+  }, 1000)
   return (
     <div>
       <button onClick={() => setCount((prev) => prev + 1)}>+</button>
