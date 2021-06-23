5108dc44348717a3438724ae210c3a594f937118
ClausWong <726787295@qq.com>
2021-06-23 17:27:19 +0800

    自定义hooks: useAsync不太理解，有点小复杂，加上今天没状态

---
+++ src/pages/HooksDiy/useAsync/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import useAsync from './useAsync';
+
+export default function UserList() {
+  // 通过 useAsync 这个函数，只需要提供异步逻辑的实现
+  const {
+    execute: fetchUsers,
+    data: users,
+    loading,
+    error,
+  } = useAsync(async () => {
+    const res = await fetch("https://reqres.in/api/users/");
+    const json = await res.json();
+    return json.data;
+  });
+
+  return null
+}
