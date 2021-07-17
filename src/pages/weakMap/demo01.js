c4626a0a4b39ac82b16f0d860c7f674d30e289f6
ClausWong <726787295@qq.com>
2021-07-17 21:08:41 +0800

    WeakMap特性

--- src/pages/weakMap/demo01.js
+++ src/pages/weakMap/demo01.js
@@ -12,3 +12,10 @@ console.log(wm.get(b))
 console.log(map.get(b))
 
 console.log('map', map.keys())
+
+/**
+ * 特性：
+ * 1. key值作为【引用的对象】被清除，此WeakMap自动被垃圾回收，有利于防止内存泄漏
+ * 2. 不可枚举
+ * 3. key为object类型
+ */
